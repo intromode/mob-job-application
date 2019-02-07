b2508b976c5636ae5eba39061b311cd94d95990c
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-07 12:52:13 -0700

    fixed clicking applicant details errors from first page so that when the last page link isnt clicked, and someone wants to click the details page from the first page, it sets that pages details equal to the last user in the array of objects in storage

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -17,16 +17,22 @@ else {
 
 const searchParam = new URLSearchParams(window.location.search);//allows us to use those methods on the query string
 const nameToFind = searchParam.get('name');
-console.log(nameToFind);
 
-//needed applicants to be = [] instead of just a var because we need length
-for(let index = 0; index < applicants.length; index++) {
-    let currentApplicant = applicants[index];
-    if(currentApplicant.name === nameToFind) {
-        applicant = currentApplicant;
-        break; //otherwise once we find the name we want, we will keep looping  
+if(nameToFind) {
+    //needed applicants to be = [] instead of just a var because we need length
+    for(let index = 0; index < applicants.length; index++) {
+        let currentApplicant = applicants[index];
+        if(currentApplicant.name === nameToFind) {
+            applicant = currentApplicant;
+            break; //otherwise once we find the name we want, we will keep looping  
+        }
     }
 }
+else {
+    applicant = applicants[applicants.length - 1];
+}
+
+
 
 name.textContent = applicant.name;
 position.textContent = applicant.position;
